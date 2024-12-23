6f5a64be09fdb687a465420f307f5e83da0f4205
decafdev <devdecaf@gmail.com>
2024-12-23 09:47:44 -0600

    AppointmentsDayView renders a div with the right id

--- Chapter01/Start/src/Appointment.tsx
+++ Chapter01/Start/src/Appointment.tsx
@@ -1,3 +1,5 @@
 import React from "react";
 
+export const AppointmentsDayView = () => <div id="appointmentsDayView"></div>;
+
 export const Appointment = ({ customer }) => <div>{customer.firstName}</div>;
--- Chapter01/Start/test/Appointment.test.tsx
+++ Chapter01/Start/test/Appointment.test.tsx
@@ -1,7 +1,7 @@
 import React, { act } from "react";
 import ReactDOM from "react-dom/client";
 import { beforeEach, describe, expect, it } from "vitest";
-import { Appointment } from "../src/Appointment";
+import { Appointment, AppointmentsDayView } from "../src/Appointment";
 
 describe("Appointment", () => {
   let container: HTMLDivElement;
@@ -24,5 +24,22 @@ describe("Appointment", () => {
     const customer = { firstName: "Jordan" };
     render(<Appointment customer={customer} />);
     expect(document.body.textContent).toContain(customer.firstName);
+  });
+});
+
+describe("AppointmentsDayView", () => {
+  let container: HTMLDivElement;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+
+  const render = (component: React.JSX.Element) =>
+    act(() => ReactDOM.createRoot(container).render(component));
+
+  it("renders a div with the right id", () => {
+    render(<AppointmentsDayView />);
+    expect(document.querySelector("div#appointmentsDayView")).not.toBeNull();
   });
 });
